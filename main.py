bbfb3d12a0043047e2995756e08e373d3d6dc3db
Rahul Ravi Prakash <eminentrahul@rocketmail.com>
2017-12-23 18:05:44 +0530

    Update main.py
    minor changes done

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import random
-n = int(input("Enter the lenght of words: "))
-w = int(input("Enter the number of words: "))
-fl = input("Enter the first letter of the word: ")
+lengthOfWord = int(input("Enter the lenght of words: "))
+numberOfWord = int(input("Enter the number of words: "))
+firstLetter = input("Enter the first letter of the word: ")
 lastLetter = input("Enter the last letter of the word: ")
 
 wordList = []
 
 for word in open('words_alpha.txt'):
-    if len(word) == n+1 and word[0] == fl and word[-2] == lastLetter:
-        wordList.append(word[:n])
+    if len(word) == lengthOfWord+1 and word[0] == firstLetter and word[-2] == lastLetter:
+        wordList.append(word[:lengthOfWord])
 
-print(random.sample(wordList, w))
+print(random.sample(wordList, numberOfWord))
                     
